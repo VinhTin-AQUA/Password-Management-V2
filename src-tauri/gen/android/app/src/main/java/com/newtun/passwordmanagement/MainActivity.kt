86603887f2ec3f864c2a8c7db3f512590d476df8
VinhTin-AQUA <tinhovinh@gmail.com>
2025-12-04 21:53:40 +0700

    fix bug

--- src-tauri/gen/android/app/src/main/java/com/newtun/passwordmanagement/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/newtun/passwordmanagement/MainActivity.kt
@@ -7,26 +7,26 @@ import androidx.core.view.WindowInsetsControllerCompat
 import androidx.core.view.ViewCompat
 
 class MainActivity : TauriActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    // Allow content to extend under the system bars
-    WindowCompat.setDecorFitsSystemWindows(window, false)
+        // Allow content to extend under the system bars
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
-    val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
 
-    windowInsetsController.apply {
-        show(WindowInsetsCompat.Type.systemBars())  // luôn hiển thị
-        systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_DEFAULT  // không ẩn tự động
-    }
+        windowInsetsController.apply {
+            show(WindowInsetsCompat.Type.systemBars())  // luôn hiển thị
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_DEFAULT  // không ẩn tự động
+        }
 
-    window.statusBarColor = android.graphics.Color.RED  // ví dụ status bar màu đen
-    window.navigationBarColor = android.graphics.Color.BLACK  // navigation bar màu đỏ
+        window.statusBarColor = android.graphics.Color.RED  // ví dụ status bar màu đen
+        window.navigationBarColor = android.graphics.Color.BLACK  // navigation bar màu đỏ
 
 
-    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
-        window.attributes.layoutInDisplayCutoutMode =
-            android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+            window.attributes.layoutInDisplayCutoutMode =
+                android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_NEVER
+        }
     }
 }
